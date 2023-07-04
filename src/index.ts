f9d032540e7361907a4abdf0d7dd7d83390758ba
Nicolas LENGLET <nicolas.lenglet35@gmail.com>
2023-07-04 06:33:39 +0200

    -On a leaflet map: display shapefile layers (based on .zip files) and corresponding information from form answers (form.geojson) as on-click popup, with layer control panel

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,43 @@ let load_map = async function(): Promise<void> {
 		zoom: 2
 	});
 
-	let tile_layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	let osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	});
-	tile_layer.addTo(lmap);
+	// osm.addTo(lmap);
 
-	let regions_geojson = await axios.get('/data/regions.geojson')
-	var geojsonLayer = new L.GeoJSON(regions_geojson.data);
-	geojsonLayer.addTo(lmap);
+	// let regions_geojson = await axios.get('/data/regions.geojson')
+	// var geojsonLayer = new L.GeoJSON(regions_geojson.data);
+	// geojsonLayer.addTo(lmap);
 	
-	let satelliteLayer = L.imageOverlay('data/landsat.png', [[44.70723, -0.784355], [44.968463, -0.3957438]]);
-	satelliteLayer.addTo(lmap);
-	// console.log(satelliteLayer);
+	// let satelliteLayer = L.imageOverlay('data/landsat.png', [[44.70723, -0.784355], [44.968463, -0.3957438]]);
+	// satelliteLayer.addTo(lmap);
+	// // console.log(satelliteLayer);
 
-	let departements_geojson = await shp(`${window.location.href}${'/data/departement.zip'}`) as GeoJSON.FeatureCollection
-	// console.log(departements_geojson)
-	var departements_geojson_layer = new L.GeoJSON(departements_geojson);
-	departements_geojson_layer.addTo(lmap);
+	let metadata_to_shp = Object({
+		"CaracteristiquesFoncierNicolasTest2": "/data/shp/departement.zip",
+		"CaracteristiquesFoncierEssai2": "/data/shp/ne_10m_airports.zip"
+	})
+
+	let metadata = (await axios.get("/data/form_res.json")).data
+	let fields = L.layerGroup()
+	metadata.features.forEach(async (feature: { id: string | number; properties: any }) => {
+		let zipshp_filename = metadata_to_shp[feature.id]
+		let geojson = await shp(`${window.location.href}${zipshp_filename}`) as GeoJSON.FeatureCollection
+		// console.log(departements_geojson)
+		var geojson_layer = new L.GeoJSON(geojson);
+		geojson_layer.bindPopup('<p>You are here ' + "username" + '</p>' + feature.properties.bf_nomfiche);
+		fields.addLayer(geojson_layer);
+	});
+	fields.addTo(lmap);
+
+	let baseMaps = {
+		"OpenStreetMap": osm
+	};
+	let overlayMaps = {
+		"Parcellaires": fields
+	};
+	L.control.layers(baseMaps, overlayMaps).addTo(lmap);
 }
 
 load_map();
